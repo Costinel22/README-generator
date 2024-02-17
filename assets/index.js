09711d137d630d465cae4f16305458c4b85ec777
Costinel22 <malaiasi_costinel@yahoo.com>
2024-02-17 20:32:26 +0000

    add on generateMarkdown structure

--- assets/index.js
+++ assets/index.js
@@ -3,51 +3,47 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+
 // array of questions for user
-const questions = [{
+const questions = () =>
+    inquirer.prompt([{
         type: 'input',
         name: 'title',
         message: 'What is your project name?',
-    },
-    {
+    }, {
         type: 'input',
         name: 'description',
         message: 'Please describe the project in a few words?',
-    },
-    {
+    }, {
         type: 'input',
         name: 'installation',
         message: 'What steps do I have to follow to install the project?',
-    },
-    {
+    }, {
         type: 'input',
         name: 'usage',
         message: 'Give me some instructions how to use it, for example?',
-    },
-    {
+    }, {
         type: 'input',
         name: 'license',
         message: 'What kind of license should the project have?',
-    },
-    {
+    }, {
         type: 'input',
-        name: 'icontributing',
+        name: 'contributing',
         message: 'How could anyone contribute to this project?',
-    },
-    {
+    }, {
         type: 'input',
         name: 'tests',
         message: 'Give me some instructions how to use it, for example?',
-    },
-    {
+    }, {
         type: 'input',
         name: 'questions',
         message: 'If you have questions I am more that hapy to try to answer?',
-    }
-];
+    }]);
+
 
 // function to write README file
 function writeToFile(fileName, data) {}
+
 
 // function to initialize program
 function init() {
@@ -56,3 +52,4 @@ function init() {
 
 // function call to initialize program
 init();
+questions()
--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -1,7 +1,38 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    return `# ${data.title}
 
+  
+  ## Table of Contents
+  
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  ${data.questions}
 `;
 }
 
