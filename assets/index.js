326745ac9e329662ba1ec1dba30a01d5f789d3cd
Costinel22 <malaiasi_costinel@yahoo.com>
2024-02-19 18:58:44 +0000

    add licence, and functions

--- assets/index.js
+++ assets/index.js
@@ -5,8 +5,7 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 
 // array of questions for user
-const questions = () =>
-    inquirer.prompt([{
+const questions = [{
         type: 'input',
         name: 'title',
         message: 'What is your project name?',
@@ -22,11 +21,14 @@ const questions = () =>
         type: 'input',
         name: 'usage',
         message: 'Give me some instructions how to use it, for example?',
-    }, {
-        type: 'input',
+    },
+    {
+        type: 'list',
         name: 'license',
-        message: 'What kind of license should the project have?',
-    }, {
+        message: 'What license would you like to include?',
+        choices: ['MIT', 'GPL', 'CC--0', 'ODbL'],
+    },
+    {
         type: 'input',
         name: 'contributing',
         message: 'How could anyone contribute to this project?',
@@ -36,20 +38,34 @@ const questions = () =>
         message: 'Give me some instructions how to use it, for example?',
     }, {
         type: 'input',
-        name: 'questions',
-        message: 'If you have questions I am more that hapy to try to answer?',
-    }]);
+        name: 'userName',
+        message: 'Wath is you github user name?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Wath is you email?',
+    },
+];
+
 
 
 // function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), function(error) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log("Now you hawe a nice readme!");
+    });
+}
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+        .then((data) => {
+            writeToFile('README.md', data);
+        });
 }
-
 // function call to initialize program
 init();
-questions()
